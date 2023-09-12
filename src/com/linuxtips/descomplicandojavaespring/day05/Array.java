6f24f0d610d567b8f20b8ca946cf68b2ff635b36
Mateus Souza da Silva <mma.teuzzz@gmail.com>
2023-09-11 21:04:47 -0300

    update Array class

--- src/com/linuxtips/descomplicandojavaespring/day05/Array.java
+++ src/com/linuxtips/descomplicandojavaespring/day05/Array.java
@@ -21,21 +21,45 @@ public class Array {
         Integer[] arrayDeInt4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}; // Preenchendo o array com 10 elementos
 
         int tamanhoDoArray = arrayDeInt4.length; // .lenght para descobrir o tamanho do array
-        /*System.out.println("Tamanho do Array: " + tamanhoDoArray);*/
+        System.out.println("Tamanho do Array: " + tamanhoDoArray);
 
         List<Integer> itens = Arrays.asList(arrayDeInt4); // Percorrendo o array como List
         itens.forEach(System.out::println);
 
         String[] treinamento = new String[]{"Treinamento", "Descomplicando", "Java", "e", "Spring", "da", "LINUXtips"};
-        String[] treinamentoNome = Arrays.copyOfRange(treinamento, from: 1, to: 5);
-        String[] linuxTipsNome = Arrays.copyOfRange(treinamento, from: 6, to: 5);
+        String[] treinamentoNome = Arrays.copyOfRange(treinamento, 1, 5);
+        // String[] linuxTipsNome = Arrays.copyOfRange(treinamento, 2, 4);
         List<String> treinamentoNomeList = List.of(treinamento);
         List<String> treinamentoList = List.of(treinamentoNome);
         treinamentoList.forEach(System.out::println);
         treinamentoNomeList.forEach(System.out::println);
 
-        String[] arrayCom3 = Arrays.copyOf(treinamentoNome, newLength:3);
+        String[] arrayCom3 = Arrays.copyOf(treinamentoNome, 3);
         List<String> arrayCom3List = List.of(arrayCom3);
         arrayCom3List.forEach(System.out::println);
+
+        /* Preenchendo um array com método fill */
+        String[] java = new String[10];
+        Arrays.fill(java, "java");
+        List<String> javaList = List.of(java);
+        javaList.forEach(System.out::println);
+
+        /* Comparando dois array com método equals */
+        int[] arrayDeInteiros = {
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        };
+
+        int[] outroArrayDeInteiros = {
+                1, 2, 3, 4, 5, 6, 7, 8, 9
+        };
+
+        System.out.println("Os arrays são iguais? " + Arrays.equals(arrayDeInteiros, outroArrayDeInteiros));
+
+        /* Ordenando um array com método sorted */
+
+        String[] sorted = Arrays.copyOf(treinamento, 8);
+        Arrays.sort(sorted);
+        List<String> sortedList = List.of(sorted);
+        sortedList.forEach(System.out::println);
     }
 }
